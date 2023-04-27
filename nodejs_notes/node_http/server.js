e0aa68582d0c84d8c368b8479593eaead565a2db
berkerkls <berkerkelesoglu@gmail.com>
2023-04-27 16:13:43 +0300

    nodemon and returning data notes

--- nodejs_notes/node_http/server.js
+++ nodejs_notes/node_http/server.js
@@ -1,12 +1,29 @@
 const http = require('http');
 
+const todos = [
+  { name: 'John Doe', age: 23 },
+  { name: 'Jane Doe', age: 25 },
+];
 // "createServer" function accept the request and gives us a response in server side backend.
 const server = http.createServer((req, res) => {
   // We can reach headers url and other parameters inside of req by destructring
   const { headers, url, method } = req;
-  console.log(headers, url, method);
-  // We are ending our request by using end() function. if we aren't, request will hanging on continuously and there will be no response.
-  res.end();
+  // We can set header some parameters by setHeader function
+  res.setHeader('Content-Type', 'application/json');
+  // This is header parameter is not usual but no harm to know
+  res.setHeader('X-Powered-By', 'Node.js');
+
+  // with write() if you want to output as a html element you should set Content-Type as text/html inside of header and you can use setHeader func as above.
+  // res.write('<h1>Hello</h1>');
+  // res.write('<h1>Hello World</h1>');
+
+  // We are ending our request by using end() function. if we aren't, request will hanging on continuously and there will be no response. Also if you return only one data you can return it inside of end func.
+  res.end(
+    JSON.stringify({
+      isSuccess: true,
+      data: todos,
+    })
+  );
 });
 
 const PORT = 5000;
